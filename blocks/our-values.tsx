99521b3f4f02dc143327e0698e2048c0629167cd
Andrew Luca <thendrluca@gmail.com>
2020-07-24 22:26:56 +0300

    feat: add our values block

---
+++ blocks/our-values.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Container } from '../components/container'
+import { Row } from '../components/row'
+import { Column } from '../components/column'
+import { Heading } from '../components/heading'
+import civicGood from '../images/civic-good.png'
+import openKnowledge from '../images/open-knowledge.svg'
+import transparency from '../images/transparency.svg'
+import innovation from '../images/innovation.png'
+
+const values = [
+  {
+    image: civicGood,
+    title: 'Civic good',
+    description: 'Dezvoltăm pentru și împreună cu cetățenii.',
+  },
+  {
+    image: openKnowledge,
+    title: 'Open knowledge',
+    description: 'Construim un ecosistem de cunoștințe transferabile.',
+  },
+  {
+    image: transparency,
+    title: 'Transparență',
+    description: 'Suntem pentru nimic de ascuns.',
+  },
+  {
+    image: innovation,
+    title: 'Inovație',
+    description: 'acem imposibilul posibil și nu ne limităm la tradițional.',
+  },
+]
+
+export function OurValues() {
+  return (
+    <section className="bg-gray-200 py-32">
+      <Container>
+        <Heading tag="h1" className="text-center text-3xl mb-16">
+          Valorile noastre
+        </Heading>
+        <Row>
+          {values.map(value => (
+            <Column className="w-full sm:w-1/2 mb-3 sm:mb-6 lg:w-1/4 ">
+              <div className="bg-white h-full">
+                <div className="relative" style={{ paddingBottom: '100%' }}>
+                  <div className="absolute inset-0 flex items-center justify-center">
+                    <img src={value.image} alt={value.title} />
+                  </div>
+                </div>
+                <div className="px-6 pb-8">
+                  <h4 className="font-bold text-xl text-blue-700 text-center mb-6">{value.title}</h4>
+                  <p className="text-center md:text-left text-lg text-gray-600">{value.description}</p>
+                </div>
+              </div>
+            </Column>
+          ))}
+        </Row>
+      </Container>
+    </section>
+  )
+}
--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { Solutions } from '../blocks/solutions'
 import { Header } from '../blocks/header'
-import { GetInvolved } from '../blocks/get-involved'
-import { Partners } from '../blocks/partners'
 import { Footer } from '../blocks/footer'
 import Head from 'next/head'
 import { AboutUsHero } from '../blocks/about-us-hero'
+import { OurValues } from '../blocks/our-values'
 
 export default function Home() {
   return (
@@ -15,6 +13,7 @@ export default function Home() {
       </Head>
       <Header />
       <AboutUsHero />
+      <OurValues />
       <Footer />
     </React.Fragment>
   )
