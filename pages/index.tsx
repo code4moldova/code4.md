16e768d9f04a146ffd19b0b4e86713f87f932511
Andrew Luca <thendrluca@gmail.com>
2020-07-24 16:44:15 +0300

    feat: add page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,14 @@ import { Hero } from '../blocks/hero'
 import { GetInvolved } from '../blocks/get-involved'
 import { Partners } from '../blocks/partners'
 import { Footer } from '../blocks/footer'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <React.Fragment>
+      <Head>
+        <title>Code 4 Moldova</title>
+      </Head>
       <Header />
       <Hero />
       <Solutions />
