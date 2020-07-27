e66180561ae49dbdcd81d5d79e01c671d2a8748f
Mosnoi Ion <johnsmithm@users.noreply.github.com>
2020-07-27 21:25:03 +0300

    feat: add comunity, legal and sponsor pages (#1)
    * added comunitate legal sponsor pages
    
    * remove Dockerfile
    
    * remove Dockerfile

---
+++ pages/comunitate.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Header } from '../blocks/header'
+import { Footer } from '../blocks/footer'
+import Head from 'next/head'
+import { Heading } from '../components/heading'
+import { Volunteers } from '../blocks/volunteers'
+import { Staff } from '../blocks/staff'
+import { BeWithUs } from '../blocks/be-with-us'
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Comunitate / Code 4 Moldova</title>
+      </Head>
+      <Header />
+      <Staff />
+      <Volunteers />
+      <BeWithUs />
+      <Footer />
+    </React.Fragment>
+  )
+}
