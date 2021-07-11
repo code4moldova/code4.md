cd00f779d63d58fb7387d30042fc085ea8e4bcfe
Andrew Luca <thendrluca@gmail.com>
2021-07-11 18:33:44 +0300

    feat: add social network redirects

--- next.config.js
+++ next.config.js
@@ -3,10 +3,42 @@ const withImages = require('next-images')
 
 const withTM = transpileModules(['hast-util-sanitize'])
 
-module.exports = withTM(
-  withImages({
-    future: {
-      webpack5: true,
+const config = {
+  future: {
+    webpack5: true,
+  },
+  redirects: async () => [
+    {
+      source: '/slack',
+      destination: 'https://code4md.slack.com',
+      permanent: false,
     },
-  }),
-)
+    {
+      source: '/patreon',
+      destination: 'https://www.patreon.com/codeformoldova',
+      permanent: false,
+    },
+    {
+      source: '/facebook',
+      destination: 'https://www.facebook.com/codeformoldova',
+      permanent: false,
+    },
+    {
+      source: '/instagram',
+      destination: 'https://www.instagram.com/code4md',
+      permanent: false,
+    },
+    {
+      source: '/twitter',
+      destination: 'https://twitter.com/Code4Moldova',
+      permanent: false,
+    },
+    {
+      source: '/linkedin',
+      destination: 'https://www.linkedin.com/company/code4moldova',
+      permanent: false,
+    },
+  ],
+}
+
+module.exports = withTM(withImages(config))
